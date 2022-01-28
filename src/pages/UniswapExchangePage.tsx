64a2a8fb8b0c76601f8863cf01a14843a8da0b8a
sarem eskandary <saremeskandary@gmail.com>
2022-01-28 18:56:37 +0330

    token address added

--- src/pages/UniswapExchangePage.tsx
+++ src/pages/UniswapExchangePage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const UniswapExchangePage = () => {
-  const tokenAddress = '' //TODO add token address
+  // Variable Token  https://rinkeby.etherscan.io/token/0xa2245c99e853519b21c319310e54625e2a9b56f5?a=0x29b57E2B404357e65a4f4b46cDC43cEa05719e99
+  const tokenAddress = "0x29b57E2B404357e65a4f4b46cDC43cEa05719e99"; //TODO add token address
   return (
     <div className="card">
       <iframe
